1fb5e5aa22e9f26d1196c760a828ecccb6b57a44
bjwellsi <braidenjwells@gmail.com>
2019-08-23 07:26:37 -0600

    commit

--- View/extension/background.js
+++ View/extension/background.js
@@ -1,5 +1,15 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
-    });
-  });
+  //check for notification support
+if(window.Notification) {
+    setInterval(() => {
+        //show the notification
+        var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
+        var hour = time[1] % 12 || 12;               // The prettyprinted hour.
+        var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
+        chrome.notifications.create('fuck my but', {
+            type: 'basic',
+            iconUrl: 'images/get_started16.png',
+            message: 'some words.',
+            title: 'yoooo'
+          });
+    }, 10000)
+};
