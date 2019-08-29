ca3356286edb991307fc7e2a35edd0b01d2e28ed
bjwellsi <braidenjwells@gmail.com>
2019-08-29 09:24:55 -0600

    restructured popup

--- View/extension/popup.js
+++ View/extension/popup.js
@@ -10,24 +10,28 @@ $(document).ready(function () {
     let listQuotes = $('#list_quotes');
     console.log($(listQuotes).val())
     $(listQuotes).on("click", () => {
+        $('#remove_quotes').show()
         //this url will eventually need to include the signed in user
         fetch('http://localhost:8080/quotes/?user=me')
             .then((res) => {
                 return res.json()
             }).then((data) => {
                 const quotesDiv = $('#quote_selection');
-                const list = document.createElement('ol');
-                let i = 0;
-                let ret = data.ret.map(row => {
-                    let line = document.createElement('li');
-                    row.listLine = i;
+                let list = $('#quote_list');
+                data.ret.forEach(row => {
+                    //let line = (`<li class="quote" value="${row.qid}>"${row.quote}" -${row.author}</li>`);
+                    list.append('<li class="quote" value="' + row.qid +'">"' + row.quote + '" -' + row.author + '</li>');
+                    /*line.text(`"${row.quote}" -${row.author}`);
+                    line.val(row.qid);
+                    list.append(line);
+                    /*row.listLine = i;
                     i++;
                     let lineData = document.createTextNode(`"${row.quote}" -${row.author}`);
                     line.append(lineData);
-                    list.append(line);
+                    list.append(line);*/
                 });
+                $('#remove_quote').css({'visibility' : 'visible'})
                 //todo make sure ret is getting initialized
-                quotesDiv.append(list);
             })
     })
 
@@ -38,31 +42,13 @@ $(document).ready(function () {
         let author = $('#author');
         let user = 'me';
         //change this eventually
-        fetch(`http://localhost:8080/addQuote/?user=${user}&quote=${quoteToAdd.value}&author=${author.value}`)
+        fetch(`http://localhost:8080/addQuote/?user=${user}&quote=${quoteToAdd.val()}&author=${author.val()}`)
             .then(res => {
                 return res.json()
             }).then(data => {
                 let para = $('#after_add_quote');
                 if (data.succeeded == "true") para.text("Success!");
                 else para.text("Failed!")
-                console.log(data)
-            })
-    })
-
-    let removeQuote = $('#add_quote');
-    removeQuote.on('click', () => {
-        //use the fetch url to add a member
-        let quoteToAdd = $('#quote_to_add');
-        let author = $('#author');
-        let user = 'me';
-        //change this eventually
-        fetch(`http://localhost:8080/addQuote/?user=${user}&quote=${quoteToAdd.value}&author=${author.value}`)
-            .then(res => {
-                return res.json()
-            }).then(data => {
-                let para = $('#after_remove_quote');
-                if (data.succeeded == "true") para.val("Success!");
-                else para.val("Failed!")
                 console.log(data)
             })
     })
